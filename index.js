a38e8660c05d197baa857445cac18a22f24b1551
Someone112113 <62203526+Someone112113@users.noreply.github.com>
2022-09-10 11:25:26 +1000

    ...

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ function randomMeal() {
       img.src = meal.strMealThumb;
       const p = div.querySelector("p");
       p.innerHTML = meal.strMeal;
+
+      const ingredients = document.querySelector("div.ingredients");
+      ingredients.innerHTML = "";
+      const ingredient1 = document.createElement("p");
+      ingredient1.innerHTML = meal.strIngredient1;
+      ingredients.appendChild(ingredient1);
+      const ingredient2 = document.createElement("p");
+      ingredient2.innerHTML = meal.strIngredient2;
+      ingredients.appendChild(ingredient2);
+      const ingredient3 = document.createElement("p");
+      ingredient3.innerHTML = meal.strIngredient3;
+      ingredients.appendChild(ingredient3);
     });
 }
 fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
