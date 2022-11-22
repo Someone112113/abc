d300829eb1d0ffe514ceba640468b77bb100757b
Someone112113 <62203526+Someone112113@users.noreply.github.com>
2022-11-22 17:21:03 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,67 +1,9 @@
-function toggleCategory() {
-  const categories = document.querySelector("#categories");
-  //if (window.getComputedStyle(categories).display === "none") {
-  if (categories.style.display != "block") {
-    categories.style.display = "block";
-  } else {
-    categories.style.display = "none";
-  }
-}
-
-async function randomMeal() {
-  const status = document.querySelector("div#status");
-  status.style.backgroundColor = "yellow";
-  status.innerHTML = "loading file";
-
-  await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
-    .then((response) => response.json())
-    // {
-    //   var test = response.json();
-    //   console.log(test);
-    // }
-    .then((data) => {
-      const meal = data.meals[0];
-      const div = document.querySelector("div.random-meal");
-      const img = div.querySelector("img");
-      img.src = meal.strMealThumb;
-      const h2 = div.querySelector("h2");
-      h2.innerHTML = meal.strMeal;
-
-      const ingredients = document.querySelector("div.ingredients");
-      ingredients.innerHTML = "<h3>Ingredients</h3>";
-      const unorderedList = document.createElement("ul");
-      for (const [key, value] of Object.entries(meal)) {
-        if (key.includes("strIngredient") && !(!value || value.length === 0)) {
-          const ingredient = document.createElement("li");
-          ingredient.innerHTML = value;
-          unorderedList.appendChild(ingredient);
-        }
-      }
-      ingredients.appendChild(unorderedList);
-
-      const instructions = document.querySelector("div.instructions");
-      instructions.innerHTML = "<h3>Instructions</h3>";
-      const instruction = document.createElement("p");
-      instruction.innerHTML = meal.strInstructions;
-      instructions.appendChild(instruction);
-    });
-
-  status.style.backgroundColor = "red";
-  status.innerHTML = "file loaded";
-}
-
-fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+fetch("https://www.themealdb.com/api/json/v1/1/random.php")
   .then((response) => response.json())
+  // {
+  //   var test = response.json();
+  //   console.log(test);
+  // }
   .then((data) => {
-    const categories = document.querySelector("#categories");
-    data.categories.forEach((category) => {
-      const li = document.createElement("li");
-      const img = document.createElement("img");
-      img.src = category.strCategoryThumb;
-      const p = document.createElement("p");
-      p.innerHTML = category.strCategory;
-      li.appendChild(img);
-      li.appendChild(p);
-      categories.appendChild(li);
-    });
+    console.log(data);
   });
